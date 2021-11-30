f986b3219febc5f451f469ccae58412d376d0654
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2021-11-30 08:53:42 +0000

    Improve database design: Use BIGINT on primary keys
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

---
+++ database/migrations/2021_11_30_010000_make_primary_key_bigint_all_tables.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace IgniterLabs\Webhook\Database\Migrations;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class MakePrimaryKeyBigintAllTables extends Migration
+{
+    public function up()
+    {
+        foreach ([
+            'igniterlabs_webhook_logs' => 'id',
+            'igniterlabs_webhook_outgoing' => 'id',
+        ] as $table => $key) {
+            Schema::table($table, function (Blueprint $table) use ($key) {
+                $table->unsignedBigInteger($key, TRUE)->change();
+            });
+        }
+    }
+
+    public function down()
+    {
+    }
+}
