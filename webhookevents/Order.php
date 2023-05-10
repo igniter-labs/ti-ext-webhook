5515441f40a3a08c71cabd3e052f9ef46213ad53
saleem <salimkamboh@gmail.com>
2023-05-10 10:47:02 +0500

    Added menu options to order payload for webhook

--- webhookevents/Order.php
+++ webhookevents/Order.php
@@ -40,8 +40,35 @@ class Order extends BaseEvent
         if (!$order instanceof Orders_model)
             return;
 
+        $options = self::getMenuOptions($order);
         return [
-            'order' => $order->toArray(),
+            'order' => $order->toArray() + ['menu_options' => $options],
         ];
+    }
+    public static function getMenuOptions($order)
+    {
+        $items = [];
+        foreach ($order->getOrderMenusWithOptions() as $orderItem) {
+            $itemOptionGroup = $orderItem->menu_options->groupBy('order_option_category');
+            if ($itemOptionGroup->isNotEmpty()) {
+                $options = [];
+                foreach ($itemOptionGroup as $itemOptionGroupName => $itemOptions) {
+                    if ($itemOptionGroup->isNotEmpty()) {
+                        $options2 = [];
+                        foreach ($itemOptions as $itemOption) {
+                            $options2[] = [
+                                'name' => $itemOption->order_option_name, 
+                                'unit_price' => $itemOption->order_option_price,
+                                'quantity' => $itemOption->quantity, 
+                                'subtotal' => currency_format($itemOption->quantity * $itemOption->order_option_price),
+                            ];
+                        }
+                        $options[] = ['name' => $itemOptionGroupName, 'details' => $options2];
+                    }
+                }
+            }
+            $items[] = ['name' => $orderItem->name, 'subtotal' => $orderItem->subtotal, 'quantity' => $orderItem->quantity, 'options' => $options];
+        }
+        return $items;
     }
 }
