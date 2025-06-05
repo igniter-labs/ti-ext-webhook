5a69c525b5347793d8488f77b6bc29d58d70b5b0
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-06-05 14:31:27 +0100

    feat: update webhook payloads for reservation, table, menu, order, customer, and category with detailed object structures
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- resources/views/_partials/setup/menu.blade.php
+++ resources/views/_partials/setup/menu.blade.php
@@ -19,19 +19,164 @@ The `stock_updated` action will ONLY be triggered during the checkout process
 | Key              | Type     | Description                                                  |
 | ---------------- | -------- | ------------------------------------------------------------ |
 | `action`         | `string` | The event type performed. Can be one of: `created` - A new menu item was created, `updated` - The menu item information was updated, `stock_updated` - The menu item stock was updated, `deleted` - An existing menu item was deleted. |
-| `menu`       | `object` | The menu item itself                                          |
+| `menu`       | `object` | The menu item object                                          |
 
 ### Webhook payload example
 
 ```json
 {
-  "action": "created",
-  "menu": {
-    "special": {},
-    "mealtime": {},
-    "categories": [],
-    "menu_options": [],
-    "locations": []  
-  }
+    "action": "updated",
+    "menu": {
+        "menu_id": 88,
+        "menu_name": "Roasted Chicken",
+        "menu_description": "Rem sint qui offici",
+        "menu_price": 8.94,
+        "minimum_qty": 1,
+        "menu_status": 1,
+        "menu_priority": 0,
+        "order_restriction": [
+            "collection"
+        ],
+        "created_at": "2025-06-05T11:55:36.000000Z",
+        "updated_at": "2025-06-05T11:56:53.000000Z",
+        "is_pending_review": 0,
+        "stock_qty": 0,
+        "locations": [
+            {
+                "location_id": 1,
+                "location_name": "Default",
+                "location_email": "admin@domain.tld",
+                "description": null,
+                "location_address_1": "24 Restaurant Pl",
+                "location_address_2": null,
+                "location_city": "Chelmsford",
+                "location_state": null,
+                "location_postcode": "CM1 4UG",
+                "location_country_id": null,
+                "location_telephone": "8765456789",
+                "location_lat": 51.3894172,
+                "location_lng": 0.1845193,
+                "location_radius": null,
+                "location_status": 1,
+                "permalink_slug": "default",
+                "is_default": false,
+                "created_at": "2023-07-04T20:01:05.000000Z",
+                "updated_at": "2025-06-01T11:02:43.000000Z",
+                "is_auto_lat_lng": 1,
+                "location_thumb": "https:\/\/tasty.test\/storage\/media\/attachments\/public\/66b\/260\/aab\/thumb_3_0_0_contain_48a4b15e.jpg",
+                "media": [
+                    {
+                        "id": 3,
+                        "disk": "public",
+                        "name": "66b260aab3be4631201551.jpg",
+                        "file_name": "default.jpg",
+                        "mime_type": "image\/jpeg",
+                        "size": 74240,
+                        "tag": "thumb",
+                        "custom_properties": [],
+                        "priority": 2,
+                        "created_at": "2024-08-06T17:43:06.000000Z",
+                        "updated_at": "2024-08-06T17:43:06.000000Z",
+                        "path": "https:\/\/tasty.test\/storage\/media\/attachments\/public\/66b\/260\/aab\/66b260aab3be4631201551.jpg",
+                        "extension": "jpg"
+                    }
+                ]
+            }
+        ],
+        "stocks": [],
+        "ingredients": [],
+        "media": [],
+        "menu_options": [
+            {
+                "menu_option_id": 21,
+                "option_id": 6,
+                "menu_id": 88,
+                "is_required": false,
+                "priority": 0,
+                "min_selected": 0,
+                "max_selected": 0,
+                "created_at": "2025-06-05T11:56:13.000000Z",
+                "updated_at": "2025-06-05T11:56:13.000000Z",
+                "option_name": "Toppers",
+                "display_type": "select",
+                "menu_option_values": [],
+                "option": {
+                    "option_id": 6,
+                    "option_name": "Toppers",
+                    "display_type": "select",
+                    "priority": 0,
+                    "created_at": "2024-07-10T09:17:55.000000Z",
+                    "updated_at": "2024-07-10T09:17:55.000000Z",
+                }
+            },
+            {
+                "menu_option_id": 22,
+                "option_id": 3,
+                "menu_id": 88,
+                "is_required": false,
+                "priority": 0,
+                "min_selected": 0,
+                "max_selected": 0,
+                "created_at": "2025-06-05T11:56:14.000000Z",
+                "updated_at": "2025-06-05T11:56:14.000000Z",
+                "option_name": "Size",
+                "display_type": "radio",
+                "menu_option_values": [],
+                "option": {
+                    "option_id": 3,
+                    "option_name": "Size",
+                    "display_type": "radio",
+                    "priority": 0,
+                    "created_at": "2023-07-04T20:01:05.000000Z",
+                    "updated_at": "2023-07-04T20:01:05.000000Z",
+                    "square_id": null,
+                    "square_version": null
+                }
+            }
+        ],
+        "special": {
+            "special_id": 9,
+            "menu_id": 88,
+            "start_date": "2025-07-17T21:22:00.000000Z",
+            "end_date": "2025-08-29T21:22:00.000000Z",
+            "special_price": 23,
+            "special_status": 0,
+            "type": "F",
+            "validity": "period",
+            "recurring_every": null,
+            "recurring_from": null,
+            "recurring_to": null,
+            "created_at": null,
+            "updated_at": null
+        },
+        "categories": [
+            {
+                "category_id": 1,
+                "name": "Appetizer",
+                "description": "Sed consequat, sapien in scelerisque egestas, neque nisi dapibus magna, non malesuada lectus ligula vel justo. Vestibulum felis nisi, tincidunt eu est quis, faucibus tincidunt ante.",
+                "parent_id": null,
+                "priority": 1,
+                "status": 1,
+                "nest_left": null,
+                "nest_right": null,
+                "permalink_slug": "appetizer",
+                "created_at": "2023-07-04T20:01:05.000000Z",
+                "updated_at": "2023-07-04T20:01:06.000000Z",
+                "square_id": null,
+                "square_version": null
+            }
+        ],
+        "mealtimes": [
+            {
+                "mealtime_id": 1,
+                "mealtime_name": "Breakfast",
+                "start_time": "07:00:00",
+                "end_time": "10:00:00",
+                "mealtime_status": 1,
+                "created_at": "2023-07-04T20:01:05.000000Z",
+                "updated_at": "2023-07-04T20:01:05.000000Z"
+            }
+        ]
+    }
 }
 ```
--- resources/views/_partials/setup/reservation.blade.php
+++ resources/views/_partials/setup/reservation.blade.php
@@ -18,18 +18,89 @@ This webhook event is triggered when a reservation is `created`, `updated`, `sta
 | Key                    | Type     | Description                                                  |
 | ---------------------- | -------- | ------------------------------------------------------------ |
 | `action` | `string` | The event type performed. Can be one of: `created` - A new reservation was created, `updated` - The reservation was updated, `status_added` - A new reservation status was added, `deleted` - An existing reservation was deleted. |
-| `reservation` | `object` | The reservation itself                                         |
+| `reservation` | `object` | The reservation object                                         |
 
 ### Webhook payload example
 
 ```json
 {
-  "action": "created",
-  "reservation": {
-    "status": {},
-    "customer": {},
-    "table": {},
-    "location": {}  
-  }
+    "action": "created",
+    "reservation": {
+        "table_id": 0,
+        "status_id": 8,
+        "customer_id": null,
+        "first_name": "Kirby",
+        "last_name": "Velasquez",
+        "email": "gemuho@mailinator.com",
+        "telephone": "+447960286304",
+        "location_id": 1,
+        "guest_num": 2,
+        "comment": "Aut ea ea sit volupt",
+        "reserve_date": "2025-06-26T23:00:00.000000Z",
+        "reserve_time": "03:15:00",
+        "duration": 45,
+        "hash": "5ce89d6aaf74ed54be4edd9262912f70",
+        "ip_address": "172.18.0.6",
+        "user_agent": "Mozilla\/5.0 (Macintosh; Intel Mac OS X 10.15; rv:138.0) Gecko\/20100101 Firefox\/138.0",
+        "updated_at": "2025-06-04T23:00:00.000000Z",
+        "created_at": "2025-06-04T23:00:00.000000Z",
+        "reservation_id": 65,
+        "customer_name": "Kirby Velasquez",
+        "table_name": "",
+        "reservation_datetime": "2025-06-27T02:15:00.000000Z",
+        "reservation_end_datetime": "2025-06-27T03:00:00.000000Z",
+        "status_name": "Pending",
+        "location": {
+            "location_id": 1,
+            "location_name": "Default",
+            "location_email": "admin@domain.tld",
+            "description": null,
+            "location_address_1": "24 Restaurant Pl",
+            "location_address_2": null,
+            "location_city": "Chelmsford",
+            "location_state": null,
+            "location_postcode": "CM1 4UG",
+            "location_country_id": null,
+            "location_telephone": "8765456789",
+            "location_lat": 51.3894172,
+            "location_lng": 0.1845193,
+            "location_radius": null,
+            "location_status": 1,
+            "permalink_slug": "default",
+            "is_default": false,
+            "created_at": "2023-07-04T20:01:05.000000Z",
+            "updated_at": "2025-06-01T11:02:43.000000Z",
+            "is_auto_lat_lng": 1,
+            "location_thumb": "https:\/\/tasty.test\/storage\/media\/attachments\/public\/66b\/260\/aab\/thumb_3_0_0_contain_48a4b15e.jpg",
+            "media": [
+                {
+                    "id": 3,
+                    "disk": "public",
+                    "name": "66b260aab3be4631201551.jpg",
+                    "file_name": "default.jpg",
+                    "mime_type": "image\/jpeg",
+                    "size": 74240,
+                    "tag": "thumb",
+                    "custom_properties": [],
+                    "priority": 2,
+                    "created_at": "2024-08-06T17:43:06.000000Z",
+                    "updated_at": "2024-08-06T17:43:06.000000Z",
+                    "path": "https:\/\/tasty.test\/storage\/media\/attachments\/public\/66b\/260\/aab\/66b260aab3be4631201551.jpg",
+                    "extension": "jpg"
+                }
+            ]
+        },
+        "tables": [],
+        "status": {
+            "status_id": 8,
+            "status_name": "Pending",
+            "status_comment": "Your table reservation is pending.",
+            "notify_customer": false,
+            "status_for": "reservation",
+            "status_color": "",
+            "created_at": "2023-07-04T20:01:05.000000Z",
+            "updated_at": "2023-07-04T20:01:05.000000Z"
+        }
+    }
 }
 ```
